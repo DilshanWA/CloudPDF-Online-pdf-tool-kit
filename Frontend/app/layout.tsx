dd4e5c35a61bad04db55a458820a11ce4cfc3b07
DilshanWA <dilshanpersonaluse@gmail.com>
2026-06-25 15:36:53 +0530

    Update 2.3

--- Frontend/app/layout.tsx
+++ Frontend/app/layout.tsx
@@ -88,13 +88,8 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${geist.variable} ${geistMono.variable}`}>
       <body className="flex min-h-screen flex-col">
-
-        <MaintenancePage />
-        {/* 
         <Header />
         <main className="flex-1">{children}</main> 
-      */} 
-        
         <Analytics />
       </body>
     </html>
